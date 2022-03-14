b12027c5adbca265cb35360a70440f2b531998b4
khalid-zein <zeinkhalid004@gmail.com>
2022-03-14 15:04:33 +0300

    modified page

--- main.js
+++ main.js
@@ -1,37 +1,28 @@
 $(document).ready(function(){
-  $(".image1").click(function(){
-    $(".image1").toggle();
-    $(".hidden").toggle();
-  });
- $(".hidden").click(function(){
-   $(".hidden").toggle();
-   $(".image1").toggle();
-  });
-});
-
-$(document).ready(function(){
-  $(".image2").click(function(){
-    $(".image2").toggle();
-    $(".hidden2").toggle();
-  });
- $(".hidden2").click(function(){
-   $(".hidden2").toggle();
-   $(".image2").toggle();
-  });
-});
-
-$(document).ready(function(){
-  $(".image3").click(function(){
-    $(".image3").toggle();
-    $(".hidden3").toggle();
-  });
+    $(".image1").click(function(){
+      $(".image1").toggle();
+      $(".hidden").toggle();
+    });
+    $(".hidden").click(function(){
+      $(".hidden").toggle();
+      $(".image1").toggle();
+    });
+    $(".image2").click(function(){
+      $(".image2").toggle();
+      $(".hidden2").toggle();
+    });
+    $(".hidden2").click(function(){
+      $(".hidden2").toggle();
+      $(".image2").toggle();
+    });
+    $(".image3").click(function(){
+      $(".image3").toggle();
+      $(".hidden3").toggle();
+    });
     $(".hidden3").click(function(){
       $(".hidden3").toggle();
       $(".image3").toggle();
-  });
-});
-
-$(document).ready(function(){
+    });
     $("#work1").mouseover(function(){
       $("#overlay1").show();
     }).mouseout(function(){
@@ -73,5 +64,31 @@ $(document).ready(function(){
       $("#overlay8").hide();
     });
 });
+
+const btn = document.getElementById("btn");
+
+function formReset() {
+    document.getElementById("myForm").reset();
+}
+
+btn.addEventListener("click", function(event) {
+    event.preventDefault();
+    let message = document.getElementById("message").value;
+    let email = document.getElementById("email").value;
+    let Name = document.getElementById("name").value;
+    let result = document.getElementById("result");
+    let error = document.getElementById("error");
+
+    if (Name === "" || email === "") {
+      return  error.innerHTML = "Enter your Name and Email!";
+    }else if((Name) && (email)){
+        result.innerHTML = Name + ", We have received your message, Thank you for reaching out to us";
+    }
+    formReset();
+    error.innerHTML = "";
+
+});
+
+
   
   
